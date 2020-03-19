523a0e72eaa0c989bf12d8b30f71341269292a3a
bartukaan <bartukaanpideci@gmail.com>
2020-03-19 19:32:19 +0300

    search bar

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,15 @@
+import React from 'react'; 
+import {View,Text,StyleSheet} from 'react-native';
+import SearchBar from '../components/SearchBar.js';
+
+
+const SearchScreen = () => {
+
+          return ( <View>
+          <SearchBar/>
+          <Text>SearchScreen Works!</Text></View>);
+};
+
+const styles = StyleSheet.create({});
+
+export default SearchScreen;
